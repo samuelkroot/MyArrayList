f077f5b92fe84daa48a79111ffa60f7fbae31273
samuelkroot <samuel.k.root@gmail.com>
2021-04-23 09:26:27 -0400

    Add Main to show/test all methods

--- Main.java
+++ Main.java
@@ -1,12 +1,30 @@
-import java.util.ArrayList;
 public class Main{
     public static void main(String[] args){
         System.out.println("Hello world");
-        ArrayList<String> arrList = new ArrayList<String>();
+        MyArrayList arrList = new MyArrayList();
 
-        arrList.add("test");
+        arrList.add("test0");
+        arrList.add("test1");
         arrList.add("test2");
-        arrList.add("Test3");
-        System.out.println(arrList.get(4));
-    }
+        arrList.add("test3");
+        arrList.add("add test");
+        System.out.println(arrList.asString());
+
+        System.out.println(arrList.get(2));
+
+        arrList.set(2, "new");
+        System.out.println(arrList.get(2));
+
+        arrList.remove(2);
+        System.out.println(arrList.get(2));
+
+        System.out.println(arrList.get(17));
+
+        arrList.addAll(new String[]{"banana", "cat", "dog"});
+
+        System.out.println("Current Size: " + arrList.size());
+        System.out.println("Current List: " + arrList.asString());
+        arrList.clear();
+        System.out.println("Cleared List: " + arrList.asString());
+    } // I think I got most of the important ones. I didnt do "toArray" but it's not like I can really show that super well
 }
